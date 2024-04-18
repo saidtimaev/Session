cb3f269c0b184203b4ac70c6591dbe0bba170809
saidtimaev <said.timaev@hotmail.com>
2024-04-18 16:45:20 +0200

    affichage liste des sessions dans accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(SessionRepository $sessionRepository): Response
     {
+        $sessions = $sessionRepository->findBy([],['dateDebut' => 'DESC']);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'sessions' => $sessions
         ]);
     }
+
+
+
 }
