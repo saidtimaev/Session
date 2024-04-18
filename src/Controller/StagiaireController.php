12f24d68dbdb439ce0ca9cba9c9c23434445c363
saidtimaev <said.timaev@hotmail.com>
2024-04-18 16:20:07 +0200

    affichage liste des stagiaires

---
+++ src/Controller/StagiaireController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StagiaireRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StagiaireController extends AbstractController
+{
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(StagiaireRepository $stagiaireRepository): Response
+    {
+        $stagiaires = $stagiaireRepository->findBy([],['nom'=>'ASC']);
+
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires'=> $stagiaires
+        ]);
+    }
+}
