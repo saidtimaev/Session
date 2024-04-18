fcd088df509d2081e82411d5f39c7069fcfa3582
saidtimaev <said.timaev@hotmail.com>
2024-04-18 12:01:09 +0200

    erreur affichage liste formations

---
+++ src/Controller/FormationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FormationRepository;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FormationController extends AbstractController
+{
+    #[Route('/formation', name: 'app_formation')]
+    public function index(FormationRepository $formationRepository): Response
+    {
+        $formations = $formationRepository->findBy([],["intitule" => "ASC"]);
+
+        return $this->render('formation/index.html.twig', [
+            'controller_name' => 'FormationController',
+            'formations' => $formations
+        ]);
+    }
+}
