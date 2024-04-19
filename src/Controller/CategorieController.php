58a4589e88af19c4856888f10b578c49af9f9144
saidtimaev <said.timaev@hotmail.com>
2024-04-19 09:58:56 +0200

    bootstrap tables

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategorieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categorie', name: 'app_categorie')]
+    public function index(CategorieRepository $categorieRepository): Response
+    {
+        $categories = $categorieRepository->findBy([],['nom' => 'ASC']);
+
+        return $this->render('categorie/index.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+}
