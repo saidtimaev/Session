f59c830c1e85f12c03f48b6da43aa2f5e2bfd087
saidtimaev <said.timaev@hotmail.com>
2024-04-19 11:08:28 +0200

    affichage infos session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Formation;
+use App\Entity\Session;
 use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,5 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
-    
+    #[Route('/session/{id}', name: 'show_session')]
+    public function show(Session $session): Response
+    {
+        return $this->render('session/show.html.twig', [
+            'session' => $session
+        ]);
+    }   
 }
