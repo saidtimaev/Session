443c0c6b601e9e1b9ad605360f7f46b47fc03a18
saidtimaev <said.timaev@hotmail.com>
2024-04-23 10:45:44 +0200

    ajout, modification et suppression d'une formation

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Formation;
+use App\Form\FormationType;
 use Doctrine\ORM\EntityRepository;
 use App\Repository\SessionRepository;
 use App\Repository\FormationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +25,46 @@ class FormationController extends AbstractController
         ]);
     }
 
+    #[Route('/formation/new', name:'new_formation')]
+    #[Route('/formation/{id}/edit', name:'edit_formation')]
+    // Ajout ou édition formation
+    public function new_edit( Formation $formation = null, Request $request, EntityManagerInterface $entityManager): Response{
+
+        if(!$formation){
+            $formation = new Formation();   
+        }
+
+        // Création du formulaire d'ajout ou d'édition
+        $form = $this->createForm(FormationType::class, $formation);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $formation = $form->getData();
+            // Prepare PDO
+            $entityManager->persist($formation);
+            // Execute PDO
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_formation');
+        }
+
+        return $this->render('formation/new.html.twig', [
+            'formAddFormation' => $form,
+        ]);
+    }
+
+     // Supprimer une formation
+     #[Route('/employe/{id}/delete', name:'delete_formation')]
+     public function delete(Formation $formation, EntityManagerInterface $entityManager){
+         
+         $entityManager->remove($formation);
+         $entityManager->flush();
+ 
+         return $this->redirectToRoute('app_formation');
+ 
+     }
+
     #[Route('/formation/{id}', name: 'sessionsParFormation')]
     public function sessionsParFormation(Formation $formation): Response
     {
@@ -29,4 +72,6 @@ class FormationController extends AbstractController
             'formation' => $formation
         ]);
     }
+
+   
 }
